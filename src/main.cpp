29cb1f82c6db75626bcbc3caa4af1fbb5fde1273
nydusborn <nydusborn@outlook.com>
2024-06-15 22:50:16 +0300

    test server

--- src/main.cpp
+++ src/main.cpp
@@ -2,28 +2,53 @@
 #include <asio.hpp>
 #include <thread>
 #include <nlohmann/json.hpp>
+#include <queue>
 
-std::vector<char> buff(256);
+struct Data {
+    asio::ip::tcp::socket& socket;
+    std::string message;
+};
 
-void SendHandler(const asio::error_code& ec, std::size_t bytes_transferred) {
-    if (ec) {
-        std::cout << ec.message() << std::endl;
-        return;
-    }
-    std::cout << "Sent a message" << std::endl;
-}
-
-void ReadHandler(const asio::error_code& ec, std::size_t bytes_transferred) {
-    if (ec) {
-        std::cout << ec.message() << std::endl;
-        return;
-    }
-    std::cout << "Received a message" << std::endl;
-    std::cout << buff.data() << std::endl;
-}
+std::mutex queue_lock;
+std::queue<Data> msg_queue;
+std::vector<std::shared_ptr<asio::ip::tcp::socket>> sockets;
 
 void WorkerThread(){
+    while(true){
+        Data* data = nullptr;
 
+        {
+            std::scoped_lock lock(queue_lock);
+            if (!msg_queue.empty()){
+                data = &msg_queue.front();
+                msg_queue.pop();
+            }
+        }
+
+        if(data != nullptr) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+            data->socket.send(asio::buffer("resolved\n"));
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
+}
+
+void DispatcherThread() {
+    while (true) {
+        for (auto& socket : sockets) {
+            if (socket->available() > 0){
+                std::vector<char> msg(512);
+                socket->read_some(asio::buffer(msg));
+                std::cout << "Received message: " << msg.data();
+                {
+                    std::scoped_lock lock(queue_lock);
+                    Data data = { *socket, msg.data() };
+                    msg_queue.emplace(data);
+                }
+            }
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
 }
 
 int main(int argc, char** argv) {
@@ -31,24 +56,33 @@ int main(int argc, char** argv) {
     using namespace asio::ip;
     tcp::endpoint endpoint(tcp::v4(), 4000);
     tcp::acceptor acceptor(service, endpoint);
-    tcp::socket socket(service);
-    std::cout << "[Server] Waiting for connection" << std::endl;
 
-    acceptor.accept(socket);
-    std::cout << "[Server] Accepted a connection from client" << std::endl;
+    for (int i = 0; i < 8; i++) {
+        std::thread t(WorkerThread);
+        t.detach();
+    }
+    {
+        std::thread t(DispatcherThread);
+        t.detach();
+    }
 
-
-    std::map<std::string, std::string> data = {
-            {"message", "Hello from the server"}
-    };
-
-    nlohmann::json j = data;
-    std::string msg = j.dump();
-    socket.async_send(asio::buffer(msg), SendHandler);
+    std::cout << "[Server] Started" << std::endl;
     while (true) {
-        socket.async_read_some(asio::buffer(buff), ReadHandler);
-        service.poll();
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        auto socket =std::make_shared<tcp::socket>(service);
+        std::cout << "[Server] Waiting for connection" << std::endl;
+
+        acceptor.accept(*socket);
+        std::cout << "[Server] Accepted a connection from client" << std::endl;
+
+        std::map<std::string, std::string> data = {
+                {"message", "Hello from the server"}
+        };
+
+        nlohmann::json j = data;
+        std::string msg = j.dump();
+        socket->send(asio::buffer(msg));
+
+        sockets.emplace_back(std::move(socket));
     }
 
     return 0;
