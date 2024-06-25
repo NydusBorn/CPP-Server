695157dd85b4bff9bfb7bbc6043e06463389efed
nydusborn <nydusborn@outlook.com>
2024-06-25 11:42:55 +0300

    imgui server configuration

--- server/main.cpp
+++ server/main.cpp
@@ -5,6 +5,11 @@
 #include <cryptopp/rng.h>
 #include <cryptopp/osrng.h>
 #include <cryptopp/modes.h>
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include <GLFW/glfw3.h>
+
 
 unsigned char AES_key[CryptoPP::AES::MAX_KEYLENGTH];
 unsigned char iv[CryptoPP::AES::BLOCKSIZE];
@@ -63,6 +68,43 @@ int main(int argc, char** argv) {
     int port = 4000;
     if (argc == 2) {
         port = std::atoi(argv[1]);
+    }
+    else{
+        if (!glfwInit())
+            return 1;
+        GLFWwindow* window = glfwCreateWindow(1280, 720, "Server Configuration", nullptr, nullptr);
+        if (window == nullptr)
+            return 1;
+        glfwMakeContextCurrent(window);
+        glfwSwapInterval(1); // Enable vsync
+        IMGUI_CHECKVERSION();
+        ImGui::CreateContext();
+        ImGui::StyleColorsDark();
+        ImGui_ImplGlfw_InitForOpenGL(window, true);
+        ImGui_ImplOpenGL3_Init();
+        while (!glfwWindowShouldClose(window)){
+            glfwPollEvents();
+
+            // Start the Dear ImGui frame
+            ImGui_ImplOpenGL3_NewFrame();
+            ImGui_ImplGlfw_NewFrame();
+            ImGui::NewFrame();
+
+            ImGui::InputInt("port", &port);
+            if (ImGui::Button("start")){
+                break;
+            }
+
+            ImGui::Render();
+            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+            glfwSwapBuffers(window);
+        }
+        ImGui_ImplOpenGL3_Shutdown();
+        ImGui_ImplGlfw_Shutdown();
+        ImGui::DestroyContext();
+        glfwDestroyWindow(window);
+        glfwTerminate();
     }
 
     rpc::server server(port);
