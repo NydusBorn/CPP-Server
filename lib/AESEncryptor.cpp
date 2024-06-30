f40821f14c0be8730e3e0a7317a7d8ad16706f99
Evgeniy <zhenya_sorokin_0303@mail.ru>
2024-06-30 15:09:30 +0300

    hpp and cpp in one

--- lib/AESEncryptor.cpp
+++ lib/AESEncryptor.cpp
@@ -1,6 +1,25 @@
-#include "AESEncryptor.hpp"
+#include <openssl/aes.h>
+#include <string>
+#include <vector>
 #include <cstring>
 
+class AESEncryptor {
+private:
+    AES_KEY key;
+    unsigned char original_ivec[16];  // Оригинальный IV
+    unsigned char ivec[16];           // Текущий IV
+
+    void resetIV();
+
+public:
+    inline static const int AES_KEY_LENGTH = 256;
+    inline static const int AES_BLOCK_SIZEE = 16;
+
+    AESEncryptor(const std::string& ckey, const std::string& ivec_str);
+
+    std::string encrypt(const std::string& plaintext);
+    std::string decrypt(const std::string& encrypted_text);
+};
 void AESEncryptor::resetIV() {
     memcpy(ivec, original_ivec, AES_BLOCK_SIZEE);
 }
--- lib/RSAEncryptor.cpp
+++ lib/RSAEncryptor.cpp
@@ -1,5 +1,28 @@
-#include "RSAEncryptor.hpp"
+#include <openssl/rsa.h>
+#include <openssl/pem.h>
+#include <openssl/err.h>
+#include <string>
+#include <vector>
+#include <stdexcept>
 
+class RSAEncryptor {
+private:
+    RSA* rsa_keypair;
+    int key_length;
+
+    void generateKeyPair(int key_length);
+
+public:
+    RSAEncryptor(int key_length = 2048);
+    RSAEncryptor(const std::string& key_pem, bool is_public);
+    ~RSAEncryptor();
+
+    std::string getPublicKey();
+    std::string getPrivateKey();
+    std::string encrypt(const std::string& plaintext);
+    std::string decrypt(const std::string& encrypted_text) const;
+    void loadKeyFromPEM(const std::string& key_pem, bool is_public);
+};
 void RSAEncryptor::generateKeyPair(int key_length) {
     this->key_length = key_length;
     rsa_keypair = RSA_new();
