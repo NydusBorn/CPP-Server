dbf06e3e21a98cb0eb1320fcff1c26646498a0d0
ItsRandomNameMeh <98980040+ItsRandomNameMeh@users.noreply.github.com>
2024-07-01 21:27:20 +0300

    ServerMain

--- Qt/Server/main.cpp
+++ Qt/Server/main.cpp
@@ -1,11 +1,32 @@
 #include <QApplication>
+#include <QWidget>
+#include <QLineEdit>
+#include <QLabel>
+#include <QVBoxLayout>
 #include <QComboBox>
-#include "QWindow"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QWindow w;
+
+    QWidget w;
+    QVBoxLayout *layout = new QVBoxLayout(&w);
+
+    QLabel *label = new QLabel(&w);
+    label->setText("Количество активных пользователей: ");
+    layout->addWidget(label);
+
+    QLineEdit *lineEdit = new QLineEdit(&w);
+    layout->addWidget(lineEdit);
+
+    QComboBox *comboBox = new QComboBox(&w);
+    layout->addWidget(comboBox);
+
+    // Connector *con = new Connector(this);
+    // label->setText(con.getUniqueAddresses);
+
+
     w.show();
+
     return a.exec();
 }
