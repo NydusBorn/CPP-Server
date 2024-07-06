e3cd3c688ebef2c120a7df7bc08c7f4dfe3ccc13
nydusborn <nydusborn@outlook.com>
2024-07-06 17:02:33 +0300

    Botnet Qt Client

--- Qt/Botnet/main.cpp
+++ Qt/Botnet/main.cpp
@@ -5,34 +5,137 @@
 #include "QPushButton"
 #include "QPointer"
 #include "iostream"
+#include "QLineEdit"
+#include "QValidator"
+#include "thread"
+#include "QSlider"
+#include "QLabel"
+#include "format"
+#include "QString"
+#include "../../lib/ConnectorBotnet.cpp"
 
 class BotnetServerStarter : public QWidget{
 protected:
-    std::unique_ptr<QHBoxLayout> lay;
-    std::unique_ptr<QPushButton> svb;
+    std::unique_ptr<QVBoxLayout> lay;
+    std::unique_ptr<QPushButton> start;
 public:
     BotnetServerStarter() : QWidget(){
         setWindowTitle("Server Configuration");
-        setFixedWidth(360);
+        setFixedWidth(400);
         setFixedHeight(240);
-        lay = std::make_unique<QHBoxLayout>(this);
-        svb = std::make_unique<QPushButton>("Server");
-        lay->addWidget(svb.get());
+        lay = std::make_unique<QVBoxLayout>(this);
+        start = std::make_unique<QPushButton>("Start Server");
+        lay->addWidget(start.get());
 }
+};
+
+class BotnetClient : public QWidget{
+protected:
+    std::unique_ptr<QVBoxLayout> lay;
+    std::chrono::time_point<std::chrono::system_clock> start_time;
+    int complete = 0;
+    std::vector<std::jthread> threads;
+    std::unique_ptr<QLabel> info;
+    std::unique_ptr<QLabel> stat_complete;
+    std::unique_ptr<QLabel> stat_speed;
+public:
+    BotnetClient(int thread_count, std::string address, int port) : QWidget(){
+        setWindowTitle("Client");
+        setFixedWidth(400);
+        setFixedHeight(240);
+        start_time = std::chrono::system_clock::now();
+        lay = std::make_unique<QVBoxLayout>(this);
+        lay->setAlignment(Qt::AlignCenter);
+        std::string infostr = std::format("Using {} threads for decryption at {}:{}", thread_count, address, port);
+        info = std::make_unique<QLabel>(QString(infostr.c_str()));
+        lay->addWidget(info.get());
+        stat_complete = std::make_unique<QLabel>("Completed chunks: ");
+        lay->addWidget(stat_complete.get());
+        stat_speed = std::make_unique<QLabel>("Speed: ");
+        lay->addWidget(stat_speed.get());
+        for (int i = 0; i < thread_count; i++){
+            threads.emplace_back([this, address, port](){
+                auto cl = std::make_unique<ConnectorBotnet>(address, port);
+                while (true){
+                    if (!cl->makeRequest()) break;
+                    complete++;
+                }
+            });
+        }
+        threads.emplace_back([this](){
+            while (true){
+                std::this_thread::sleep_for(std::chrono::milliseconds (1000 / 60));
+                std::chrono::time_point<std::chrono::system_clock> current_time = std::chrono::system_clock::now();
+                std::chrono::duration<double> elapsed_seconds = current_time - start_time;
+                stat_complete->setText(std::format("Completed chunks: {}", complete).c_str());
+                double speed = complete / elapsed_seconds.count();
+                if (speed < 1){
+                    stat_speed->setText(std::format("Speed: {:.2f} s/chunk", 1/speed).c_str());
+                }
+                else{
+                    stat_speed->setText(std::format("Speed: {:.2f} Chunks/s", speed).c_str());
+                }
+
+            }
+        });
+    }
 };
 
 class BotnetClientStarter : public QWidget{
 protected:
-    std::unique_ptr<QHBoxLayout> lay;
-    std::unique_ptr<QPushButton> clb;
+    std::unique_ptr<QVBoxLayout> lay;
+    std::unique_ptr<QLabel> address_label;
+    std::unique_ptr<QLineEdit> address;
+    std::unique_ptr<QLabel> port_label;
+    std::unique_ptr<QLineEdit> port;
+    std::unique_ptr<QLabel> thread_label;
+    std::unique_ptr<QSlider> thread_count;
+    std::unique_ptr<QPushButton> start;
+
+    void check_port(){
+        int port_int = port->text().toInt();
+        if(port_int > 65535){
+            port->setText("65535");
+        }
+    }
+
+    void start_client(){
+        int thread_count_int = thread_count->value();
+        std::string address_str = address->text().toStdString();
+        int port_int = port->text().toInt();
+        this->close();
+        auto conf = new BotnetClient(thread_count_int, address_str, port_int);
+        conf->show();
+    }
+
 public:
     BotnetClientStarter() : QWidget(){
         setWindowTitle("Client Configuration");
-        setFixedWidth(360);
+        setFixedWidth(400);
         setFixedHeight(240);
-        lay = std::make_unique<QHBoxLayout>(this);
-        clb = std::make_unique<QPushButton>("Client");
-        lay->addWidget(clb.get());
+        lay = std::make_unique<QVBoxLayout>(this);
+        address_label = std::make_unique<QLabel>("Input Server Address:");
+        port_label = std::make_unique<QLabel>("Input Server Port:");
+        lay->addWidget(address_label.get());
+        address = std::make_unique<QLineEdit>("127.0.0.1");
+        port = std::make_unique<QLineEdit>("4000");
+        port->setValidator(new QIntValidator(0, 65535));
+        connect(port.get(), &QLineEdit::textChanged, this, &BotnetClientStarter::check_port);
+        thread_count = std::make_unique<QSlider>(Qt::Horizontal);
+        thread_count->setMinimum(1);
+        thread_count->setMaximum(std::thread::hardware_concurrency());
+        thread_count->setValue(std::thread::hardware_concurrency());
+        thread_count->setTickPosition(QSlider::TicksBothSides);
+        thread_count->setTickInterval(1);
+        lay->addWidget(address.get());
+        lay->addWidget(port_label.get());
+        lay->addWidget(port.get());
+        thread_label = std::make_unique<QLabel>("Input Number of Threads:");
+        lay->addWidget(thread_label.get());
+        lay->addWidget(thread_count.get());
+        start = std::make_unique<QPushButton>("Start Client");
+        connect(start.get(), &QPushButton::clicked, this, &BotnetClientStarter::start_client);
+        lay->addWidget(start.get());
     }
 };
 
@@ -56,7 +159,7 @@ protected:
 public:
     BotnetStarter(QWidget *parent = nullptr) : QWidget(parent){
         setWindowTitle("Role Configuration");
-        setFixedWidth(360);
+        setFixedWidth(400);
         lay = std::make_unique<QHBoxLayout>(this);
         svb = std::make_unique<QPushButton>("Server");
         connect(svb.get(), &QPushButton::clicked, this, &BotnetStarter::configure_server);
